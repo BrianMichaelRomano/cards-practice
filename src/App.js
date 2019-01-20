dcee6dd0752fd1d99efc117302226a982068c101
Brian <brianmromano@gmail.com>
2019-01-20 18:50:18 -0500

    initial cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from './Card/Card.js';
+import rogueImg from './img/cowled.svg';
+import knightImg from './img/visored-helm.svg';
+import peasantImg from './img/farmer.svg';
+import mageImg from './img/wizard-face.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    cards: [
+      { id: "1", imgSrc: rogueImg, title: 'Appprentice Rogue', cost: 2, attack: 2, defense: 1, description: 'A Rookie rogue trying to make a name for himself. With nothing to lose... they can be the most dangerous.' },
+      { id: "2", imgSrc: knightImg, title: 'Master Knight', cost: 6, attack: 4, defense: 4, description: 'A seasoned veteran of war who has slain many beasts and gained honor among these lands.' },
+      { id: "3", imgSrc: peasantImg, title: 'Peasant', cost: 1, attack: 1, defense: 1, description: 'Worn out from farming all day...won"t be putting up much of a fight.' },
+      { id: "4", imgSrc: mageImg, title: 'Old Mage', cost: 4, attack: 5, defense: 1, description: 'He has been casting spells so long he is deadly but also about to die...' }
+    ],
+    cardSelected: null
+  };
+
+  selectCard = (e) => {
+    const cardId = e.currentTarget.id;
+    this.setState({
+      cardSelected: cardId
+    });
+    console.log(this.state)
+  }
+
   render() {
+    let cards = this.state.cards.map((card, i) => {
+      return <Card onCardClicked={(e) => this.selectCard(e)} key={card.id} cardStats={this.state.cards[i]} />
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>{this.state.cardSelected}</p>
+        {cards}
       </div>
     );
   }
---
+++ src/Card/Card.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div onClick={props.onCardClicked} className="cardWrapper" id={props.cardStats.id}>
+      <div className="card">
+        <div className="innerCard">
+          <div className="cardTitle">
+            <p>{props.cardStats.title}</p>
+          </div>
+          <div className="cardImage">
+            <img src={props.cardStats.imgSrc} alt="cardImage" />
+          </div>
+          <div className="cardStats">
+            <p>{props.cardStats.attack}/{props.cardStats.defense}</p>
+            <p>{props.cardStats.cost}</p>
+          </div>
+          <div className="cardDescription">
+            <p>{props.cardStats.description}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
